170440c860b8d4f55b861050443acb4379648baa
brasseld <brasseld@gmail.com>
2017-12-06 17:49:23 +0100

    fix(elasticsearch): Refactor search method to avoid executeBlocking
    Closes gravitee-io/issues#923

--- gravitee-repository-elasticsearch-http/src/main/java/io/gravitee/repository/elasticsearch/ElasticsearchComponent.java
+++ gravitee-repository-elasticsearch-http/src/main/java/io/gravitee/repository/elasticsearch/ElasticsearchComponent.java
@@ -25,26 +25,16 @@ import io.gravitee.repository.elasticsearch.model.elasticsearch.ESSearchResponse
 import io.gravitee.repository.elasticsearch.model.elasticsearch.Health;
 import io.gravitee.repository.elasticsearch.utils.FreeMarkerComponent;
 import io.gravitee.repository.exceptions.TechnicalException;
-import io.vertx.core.AsyncResult;
-import io.vertx.core.Handler;
 import io.vertx.core.http.HttpClientOptions;
-import io.vertx.rx.java.ObservableFuture;
-import io.vertx.rx.java.RxHelper;
-import io.vertx.rxjava.core.Context;
 import io.vertx.rxjava.core.Vertx;
 import io.vertx.rxjava.core.buffer.Buffer;
 import io.vertx.rxjava.core.http.HttpClient;
 import io.vertx.rxjava.core.http.HttpClientRequest;
-import io.vertx.rxjava.core.http.HttpClientResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import rx.Observable;
-import rx.Observer;
-import rx.Single;
 import rx.Subscriber;
-import rx.functions.Func1;
-import rx.observers.Subscribers;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
@@ -57,17 +47,16 @@ import java.util.concurrent.ExecutionException;
 
 /**
  * Utility Elasticsearch Spring bean used to call Elasticsearch using the REST api.
- * 
+ *
  * @author Guillaume Waignier
  * @author Sebastien Devaux
- *
  */
 public class ElasticsearchComponent {
 
-    /** Logger. */
-    private final Logger logger = LoggerFactory.getLogger(ElasticsearchComponent.class);
+	/** Logger. */
+	private final Logger logger = LoggerFactory.getLogger(ElasticsearchComponent.class);
 
-    //ES PATH
+	//ES PATH
 	private static final String URL_STATE_CLUSTER = "/_cluster/health";
 	private static final String URL_SEARCH = "/_search?ignore_unavailable=true";
 	private static final String URL_TEMPLATE = "/_template";
@@ -82,7 +71,7 @@ public class ElasticsearchComponent {
 	 */
 	@Autowired
 	private ElasticConfiguration configuration;
-	
+
 	/**
 	 * Templating tool.
 	 */
@@ -95,29 +84,25 @@ public class ElasticsearchComponent {
 	/**
 	 * HTTP client.
 	 */
-    private HttpClient httpClient;
+	private HttpClient httpClient;
 
-    /**
-     * JSON mapper.
-     */
-    private ObjectMapper mapper;
-    
-    /**
-     * Authorization header if Elasticsearch is protected.
-     */
-    private String authorizationHeader;
+	/**
+	 * JSON mapper.
+	 */
+	private ObjectMapper mapper;
+
+	/**
+	 * Authorization header if Elasticsearch is protected.
+	 */
+	private String authorizationHeader;
 
 	private int majorVersion;
 
-	private Context context;
-
-    /**
-     * Initialize the Async REST client.
-     */
-    @PostConstruct
+	/**
+	 * Initialize the Async REST client.
+	 */
+	@PostConstruct
 	public void start() throws ExecutionException, InterruptedException, IOException, TechnicalException {
-		context = vertx.getOrCreateContext();
-
 		if (! configuration.getEndpoints().isEmpty()) {
 			final Endpoint endpoint = configuration.getEndpoints().get(0);
 			final URI elasticEdpt = URI.create(endpoint.getUrl());
@@ -155,36 +140,27 @@ public class ElasticsearchComponent {
 	}
 
 	private int getMajorVersion() throws ExecutionException, InterruptedException, IOException, TechnicalException {
-		Observable<VertxHttpResponse> get = Observable.unsafeCreate(subscriber -> {
+		Observable<String> observable = Observable.unsafeCreate(subscriber -> {
 			HttpClientRequest req = httpClient.get("/");
 			addCommonHeaders(req);
-			Observable<VertxHttpResponse> responseObservable = req
+			req
 					.exceptionHandler(subscriber::onError)
 					.toObservable()
-					.flatMap(new Func1<HttpClientResponse, Observable<VertxHttpResponse>>() {
-						@Override
-						public Observable<VertxHttpResponse> call(HttpClientResponse httpClientResponse) {
-							return Observable.unsafeCreate(subscriber1 -> httpClientResponse.bodyHandler(body -> {
-								subscriber1.onNext(new VertxHttpResponse(httpClientResponse, body));
-								subscriber1.onCompleted();
-							}));
+					.flatMap(resp -> {
+						if (resp.statusCode() != HttpStatusCode.OK_200) {
+							subscriber.onError(new RuntimeException(
+									"Impossible to call Elasticsearch. Elasticsearch response code is " + resp.statusCode()));
 						}
-					});
+						return Observable.just(Buffer.buffer()).mergeWith(resp.toObservable());
+					})
+					.reduce(Buffer::appendBuffer)
+					.map(Buffer::toString)
+					.subscribe(subscriber);
 
-			responseObservable.subscribe(subscriber);
 			req.end();
 		});
 
-		VertxHttpResponse single = get.toBlocking().single();
-
-		if (single.response.statusCode() != HttpStatusCode.OK_200) {
-			logger.error("Impossible to call Elasticsearch GET {}.", "/");
-			throw new TechnicalException(
-					"Impossible to call Elasticsearch. Elasticsearch response code is " + single.response.statusCode() );
-		}
-
-		String body = single.body.toString();
-
+		String body = observable.toBlocking().single();
 		String version = mapper.readTree(body).path("version").path("number").asText();
 		float result = Float.valueOf(version.substring(0, 3));
 		int major = Integer.valueOf(version.substring(0, 1));
@@ -194,9 +170,9 @@ public class ElasticsearchComponent {
 
 		return major;
 	}
-	
+
 	/**
-	 * Create the Basic HTTP auth 
+	 * Create the Basic HTTP auth
 	 * @param username username
 	 * @param password password
 	 * @return Basic auth string
@@ -206,7 +182,7 @@ public class ElasticsearchComponent {
 		final String encodedAuth = Base64.getEncoder().encodeToString(auth.getBytes(StandardCharsets.UTF_8));
 		return "Basic " + encodedAuth;
 	}
-	
+
 	/**
 	 * Get the cluster health
 	 * @return the cluster health
@@ -214,35 +190,27 @@ public class ElasticsearchComponent {
 	 */
 	public Health getClusterHealth() throws TechnicalException {
 		try {
-			Observable<VertxHttpResponse> get = Observable.unsafeCreate(subscriber -> {
+			Observable<String> observable = Observable.unsafeCreate(subscriber -> {
 				HttpClientRequest req = httpClient.get(URL_STATE_CLUSTER);
 				addCommonHeaders(req);
-				Observable<VertxHttpResponse> responseObservable = req
+				req
 						.exceptionHandler(subscriber::onError)
 						.toObservable()
-						.flatMap(new Func1<HttpClientResponse, Observable<VertxHttpResponse>>() {
-							@Override
-							public Observable<VertxHttpResponse> call(HttpClientResponse httpClientResponse) {
-								return Observable.unsafeCreate(subscriber1 -> httpClientResponse.bodyHandler(body -> {
-									subscriber1.onNext(new VertxHttpResponse(httpClientResponse, body));
-									subscriber1.onCompleted();
-								}));
+						.flatMap(resp -> {
+							if (resp.statusCode() != HttpStatusCode.OK_200) {
+								subscriber.onError(new RuntimeException(
+										"Impossible to call Elasticsearch. Elasticsearch response code is " + resp.statusCode()));
 							}
-						});
+							return Observable.just(Buffer.buffer()).mergeWith(resp.toObservable());
+						})
+						.reduce(Buffer::appendBuffer)
+						.map(Buffer::toString)
+						.subscribe(subscriber);
 
-				responseObservable.subscribe(subscriber);
 				req.end();
 			});
 
-			VertxHttpResponse single = get.toBlocking().single();
-
-			if (single.response.statusCode() != HttpStatusCode.OK_200) {
-				logger.error("Impossible to call Elasticsearch GET {}.", URL_STATE_CLUSTER);
-				throw new TechnicalException(
-						"Impossible to call Elasticsearch. Elasticsearch response code is " + single.response.statusCode() );
-			}
-
-			String body = single.body.toString();
+			String body = observable.toBlocking().single();
 			logger.debug("Response of ES for GET {} : {}", URL_STATE_CLUSTER, body);
 
 			return this.mapper.readValue(body, Health.class);
@@ -250,7 +218,6 @@ public class ElasticsearchComponent {
 			logger.error("Impossible to call Elasticsearch GET {}.", URL_STATE_CLUSTER, e);
 			throw new TechnicalException("Impossible to call Elasticsearch. Error is " + e.getClass().getSimpleName(),
 					e);
-
 		}
 	}
 
@@ -264,7 +231,7 @@ public class ElasticsearchComponent {
 	public ESSearchResponse search(final String indexes, final String query) throws TechnicalException {
 		return this.search(indexes, null, query);
 	}
-	
+
 	/**
 	 * Perform an HTTP search query
 	 * @param indexes indexes names. If null search on all indexes
@@ -276,71 +243,42 @@ public class ElasticsearchComponent {
 	public ESSearchResponse search(final String indexes, final String types, final String query) throws TechnicalException {
 		try {
 			// index can be null _search on all index
-			final StringBuilder url = new StringBuilder("/").append(indexes);
+			final StringBuilder url = new StringBuilder()
+					.append('/')
+					.append(indexes);
 
 			if (types != null) {
-				url.append("/").append(types);
+				url.append('/').append(types);
 			}
 
 			url.append(URL_SEARCH);
 
 			final String queryUrl = url.toString();
 
-			Single<ESSearchResponse> responseSingle = context.rxExecuteBlocking(new Handler<io.vertx.rxjava.core.Future<ESSearchResponse>>() {
-				@Override
-				public void handle(io.vertx.rxjava.core.Future<ESSearchResponse> future) {
+			Observable<String> observable = Observable.unsafeCreate(subscriber -> {
+				HttpClientRequest req = httpClient
+						.post(queryUrl)
+						.putHeader(HttpHeaders.CONTENT_TYPE, CONTENT_TYPE);
+				addCommonHeaders(req);
+				req
+						.exceptionHandler(subscriber::onError)
+						.toObservable()
+						.flatMap(resp -> {
+							if (resp.statusCode() != HttpStatusCode.OK_200) {
+								subscriber.onError(new RuntimeException(
+										"Impossible to call Elasticsearch. Elasticsearch response code is " + resp.statusCode()));
+							}
+							return Observable.just(Buffer.buffer()).mergeWith(resp.toObservable());
+						})
+						.reduce(Buffer::appendBuffer)
+						.map(Buffer::toString)
+						.subscribe(subscriber);
 
-					HttpClientRequest req = httpClient
-							.post(queryUrl)
-							.putHeader(HttpHeaders.CONTENT_TYPE, CONTENT_TYPE);
-					addCommonHeaders(req);
-					req
-							.exceptionHandler(new Handler<Throwable>() {
-								@Override
-								public void handle(Throwable throwable) {
-									logger.error("Unexpected error: ", throwable);
-									future.fail(throwable);
-								}
-							})
-							.toObservable()
-							.subscribe(new Subscriber<HttpClientResponse>() {
-								@Override
-								public void onCompleted() {
-
-								}
-
-								@Override
-								public void onError(Throwable throwable) {
-									logger.error("Unexpected error: ", throwable);
-									future.fail(throwable);
-								}
-
-								@Override
-								public void onNext(HttpClientResponse httpClientResponse) {
-									httpClientResponse.bodyHandler(body -> {
-										if (httpClientResponse.statusCode() != HttpStatusCode.OK_200) {
-											logger.error("Impossible to call Elasticsearch POST {}.", queryUrl);
-											future.fail(new TechnicalException(
-													"Impossible to call Elasticsearch. Elasticsearch response code is " + httpClientResponse.statusCode()));
-										}
-
-										String sBody = body.toString();
-										logger.debug("Response of ES for POST {} : {}", queryUrl, sBody);
-
-										try {
-											future.complete(mapper.readValue(sBody, ESSearchResponse.class));
-										} catch (IOException ioe) {
-											future.fail(ioe);
-										}
-									});
-								}
-							});
-
-					req.end(query);
-				}
+				req.end(query);
 			});
 
-			return responseSingle.toBlocking().value();
+			String body = observable.toBlocking().single();
+			return mapper.readValue(body, ESSearchResponse.class);
 		} catch (final Exception e) {
 			logger.error("Impossible to call Elasticsearch", e);
 			throw new TechnicalException("Impossible to call Elasticsearch.", e);
@@ -363,37 +301,29 @@ public class ElasticsearchComponent {
 
 			logger.debug("PUT template : {}", template);
 
-			Observable<VertxHttpResponse> get = Observable.unsafeCreate(subscriber -> {
+			Observable<String> observable = Observable.unsafeCreate(subscriber -> {
 				HttpClientRequest req = httpClient
 						.put(templateUrl)
 						.putHeader(HttpHeaders.CONTENT_TYPE, CONTENT_TYPE);
 				addCommonHeaders(req);
-				Observable<VertxHttpResponse> responseObservable = req
+				req
 						.exceptionHandler(subscriber::onError)
 						.toObservable()
-						.flatMap(new Func1<HttpClientResponse, Observable<VertxHttpResponse>>() {
-							@Override
-							public Observable<VertxHttpResponse> call(HttpClientResponse httpClientResponse) {
-								return Observable.unsafeCreate(subscriber1 -> httpClientResponse.bodyHandler(body -> {
-									subscriber1.onNext(new VertxHttpResponse(httpClientResponse, body));
-									subscriber1.onCompleted();
-								}));
+						.flatMap(resp -> {
+							if (resp.statusCode() != HttpStatusCode.OK_200) {
+								subscriber.onError(new RuntimeException(
+										"Impossible to call Elasticsearch PUT {}: {}" + templateUrl + resp.statusCode()));
 							}
-						});
+							return Observable.just(Buffer.buffer()).mergeWith(resp.toObservable());
+						})
+						.reduce(Buffer::appendBuffer)
+						.map(Buffer::toString)
+						.subscribe(subscriber);
 
-				responseObservable.subscribe(subscriber);
 				req.end(template);
 			});
 
-			VertxHttpResponse single = get.toBlocking().single();
-
-			String body = single.body.toString();
-
-			if (single.response.statusCode() != HttpStatusCode.OK_200) {
-				logger.error("Impossible to call Elasticsearch PUT {}. Body is {}", templateUrl, body);
-				throw new TechnicalException("Impossible to call Elasticsearch PUT " + templateUrl
-						+ ". Response is " + single.response.statusCode());
-			}
+			String body = observable.toBlocking().single();
 
 			logger.debug("Response of ES for PUT {} : {}",  URL_TEMPLATE + "/gravitee", body);
 		} catch (final Exception e) {
@@ -403,51 +333,46 @@ public class ElasticsearchComponent {
 	}
 
 	public void index(final String bulk) {
-			try {
-				logger.debug("Try to call POST {}, with body {}", URL_BULK, bulk);
+		try {
+			logger.debug("Try to call POST {}, with body {}", URL_BULK, bulk);
 
-				HttpClientRequest req = httpClient.post(URL_BULK);
-				req.putHeader(HttpHeaders.CONTENT_TYPE, "application/x-ndjson");
-				addCommonHeaders(req);
+			HttpClientRequest req = httpClient
+					.post(URL_BULK)
+					.putHeader(HttpHeaders.CONTENT_TYPE, "application/x-ndjson");
+			addCommonHeaders(req);
 
-				req
-						.toObservable()
-						.flatMap(new Func1<HttpClientResponse, Observable<VertxHttpResponse>>() {
-							@Override
-							public Observable<VertxHttpResponse> call(HttpClientResponse httpClientResponse) {
-								return Observable.unsafeCreate(subscriber1 -> httpClientResponse.bodyHandler(body -> {
-									subscriber1.onNext(new VertxHttpResponse(httpClientResponse, body));
-									subscriber1.onCompleted();
-								}));
-							}
-						})
-						.subscribe(new Subscriber<VertxHttpResponse>() {
-							@Override
-							public void onCompleted() {
+			req
+					.toObservable()
+					.flatMap(resp -> {
+						if (resp.statusCode() != HttpStatusCode.OK_200) {
+							new RuntimeException(
+									"Impossible to call Elasticsearch PUT {}: {}" + URL_BULK + resp.statusCode());
+						}
+						return Observable.just(Buffer.buffer()).mergeWith(resp.toObservable());
+					})
+					.reduce(Buffer::appendBuffer)
+					.map(Buffer::toString)
+					.subscribe(new Subscriber<String>() {
+						@Override
+						public void onCompleted() {
 
-							}
+						}
 
-							@Override
-							public void onError(Throwable t) {
-								logger.error("An error occurs while calling Elasticsearch POST {}", URL_BULK, t);
-							}
+						@Override
+						public void onError(Throwable t) {
+							logger.error("An error occurs while calling Elasticsearch POST {}", URL_BULK, t);
+						}
 
-							@Override
-							public void onNext(VertxHttpResponse response) {
-								String body = response.body.toString();
+						@Override
+						public void onNext(String response) {
+							logger.debug("Response of ES for POST {} : {}", URL_BULK, response);
+						}
+					});
 
-								logger.debug("Response of ES for POST {} : {}", URL_BULK, body);
-
-								if (response.response.statusCode() != HttpStatusCode.OK_200) {
-									logger.error("Impossible to call Elasticsearch POST {}. Body is {}", URL_BULK, body);
-								}
-							}
-						});
-
-				req.end(bulk);
-			} catch (Exception ex) {
-				logger.error("Unexpected error while bulk indexing data to Elasticsearch", ex);
-			}
+			req.end(bulk);
+		} catch (Exception ex) {
+			logger.error("Unexpected error while bulk indexing data to Elasticsearch", ex);
+		}
 	}
 
 	/**
@@ -465,15 +390,6 @@ public class ElasticsearchComponent {
 		// basic auth
 		if (this.authorizationHeader != null) {
 			request.putHeader(HttpHeaders.AUTHORIZATION, this.authorizationHeader);
-		}
-	}
-
-	class VertxHttpResponse {
-		final HttpClientResponse response;
-		final Buffer body;
-		VertxHttpResponse( HttpClientResponse theResponse, Buffer theBody ) {
-			response = theResponse;
-			body = theBody;
 		}
 	}
 }
